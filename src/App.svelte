bb7de2659ee06682ff2d160844eccc6e785eb319
IcaroAndries <icaro.dev.andries@gmail.com>
2024-03-25 12:29:02 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  console.log("ao menos entrou");
+</script>
+
+<main>
+  <p>hello world!</p>
+</main>
+
+<style>
+  main {
+    width: 100%;
+    min-width: 100vw;
+
+    height: 100%;
+    min-height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background: url("/background.jpg");
+    background-size: cover;
+  }
+</style>
---
+++ src/components/MainCard/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<div class="MainCard">
+  <div class="card-content"></div>
+</div>
+
+<style>
+  .MainCard {
+    border-radius: 10px;
+    background-color: midnightblue;
+    opacity: 0.8;
+    height: 80%;
+    display: grid;
+    grid-template-columns: "2fr", "1fr 1fr";
+  }
+</style>
